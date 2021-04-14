91bc5c68d5763bca452e4386ce2db8a7c8713f47
Henrik Oldenborg <henrik.oldenborg@lego.com>
2021-04-15 01:26:50 +0200

    Add logout functionality

--- src/views/SecureArea.vue
+++ src/views/SecureArea.vue
@@ -5,7 +5,7 @@
         <h1 class="title is-1">Secure area</h1>
 
         <div class="buttons">
-          <b-button type="is-primary">Logout</b-button>
+          <b-button type="is-primary" @click="logout">Logout</b-button>
         </div>
       </div>
     </div>
@@ -13,7 +13,39 @@
 </template>
 
 <script>
+import { auth } from "@/firebase";
 export default {
-  name: "SecureArea"
+  name: "SecureArea",
+  data() {
+    return {
+      errorCode: null,
+      errorMessage: null
+    };
+  },
+  computed: {
+    err() {
+      return `${this.errorCode}: ${this.errorMessage}`;
+    }
+  },
+  methods: {
+    logout() {
+      auth
+        .signOut()
+        .then(() => {
+          // Sign-out successful.
+        })
+        .catch(error => {
+          this.errorCode = error.code;
+          this.errorMessage = error.message;
+
+          this.$buefy.toast.open({
+            duration: 5000,
+            message: this.err,
+            position: "is-bottom-right",
+            type: "is-danger"
+          });
+        });
+    }
+  }
 };
 </script>
