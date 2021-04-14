ba7532bc14bc858674845c9fc371b360cd920e56
Henrik Oldenborg <henrik.oldenborg@lego.com>
2021-04-15 00:56:24 +0200

    Start using buefy

--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
-import Vue from "vue";
-import App from "./App.vue";
 import "./registerServiceWorker";
+import "buefy/dist/buefy.css";
+
+import App from "./App.vue";
+import Buefy from "buefy";
+import Vue from "vue";
 import router from "./router";
 import store from "./store";
 
 Vue.config.productionTip = false;
+
+Vue.use(Buefy);
 
 new Vue({
   router,
